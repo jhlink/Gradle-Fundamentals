06460ec6364fc375e73c970320c217cab4ceb06b
jhlink <jhlink@users.noreply.github.com>
2018-11-26 15:08:45 -0500

    feat: Add supporting files to display jokes from Java libraries

---
+++ 4.02-Exercise-CreateAJavaLibrary/exercise/app/src/main/java/com/example/oliverh/myapplication/MainActivityFragment.java
@@ -0,0 +1,29 @@
+package com.example.oliverh.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.oliverh.javajokes.JavaSmith;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.text_view);
+        JavaSmith myJoker = new JavaSmith();
+        textView.setText(myJoker.getJoke());
+        return rootView;
+    }
+}
